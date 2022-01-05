fb5f012cdb3b33500191a08604db95533e21eeb2
Henry Zhou <henryzhou1025@outlook.com>
2022-01-05 15:21:43 +0000

    While looping

---
+++ montypython/monty_python3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+round = 0
+answer = " "
+
+while round < 3 and (answer != "brian" and answer !="shrubbery"):
+    round += 1     # increase the round counter by 1
+    answer = input('Finish the movie title, "Monty Python\'s The Life of ______": ').lower()
+    if answer == "brian": # logic to check if user gave correct answer
+        print("Correct!")
+    elif answer == "shrubbery":
+        print("You gave the super secret answer!")
+    elif round == 3:    # logic to ensure round has not yet reached 3
+        print("Sorry, the answer was Brian.")
+    else:                 # if answer was wrong
+        print("Sorry. Try again!")
+
